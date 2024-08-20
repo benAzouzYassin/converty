b9c4650928bf98eef3eecb8138aa34a2771da8a1
yassine benazouz <yassinebenazouz123@gmail.com>
2024-08-20 08:14:36 +0200

    fix loading bug

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <main className=" bg-[#0A0A0B]  h-screen">
       <div className=" flex items-center flex-col relative justify-center gap-7 h-[80vh]">
-        <p className="text-[#CECECE] text-5xl font-sans">Almost there....</p>
+        <p className="text-[#CECECE] text-4xl font-mono">Almost there...</p>
         <div className="w-[400px] border border-white/20  overflow-hidden relative">
           <progress className="progress h-full w-80"></progress>
         </div>
